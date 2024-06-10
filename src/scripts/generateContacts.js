15f8a4aa7ed9f366e551b21dacd5abf1ceb27164
CosmicSand <skyjlen@gmail.com>
2024-06-10 12:29:45 +0300

    upd

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,25 @@
+import { log } from 'console';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    // Зчитуємо початковий масив контактів
+
+    const contacts = await fs.readFile(PATH_DB);
+    const parsedContacts = JSON.parse(contacts);
+
+    // Генеруємо нові контакти
+    const newContacts = Array.from({ length: number }, createFakeContact);
+
+    //   Додаємо нові контакти до початкового масиву
+    const updatedContacts = JSON.stringify([...parsedContacts, ...newContacts]);
+    await fs.writeFile(PATH_DB, updatedContacts);
+    console.log(parsedContacts);
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+  }
+};
 
 await generateContacts(5);
