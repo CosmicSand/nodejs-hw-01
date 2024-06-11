c5c97a547752c506fceeabe35c9df93bb4fc23ad
CosmicSand <skyjlen@gmail.com>
2024-06-11 12:37:17 +0300

    upd

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -3,13 +3,18 @@ import { createFakeContact } from '../utils/createFakeContact.js';
 import fs from 'fs/promises';
 
 export const addOneContact = async () => {
+  // Зчитуємо початковий масив контактів
+
+  const contacts = await fs.readFile(PATH_DB);
+  const parsedContacts = JSON.parse(contacts);
+
   // Генеруємо новий контакт
 
   const newContact = createFakeContact();
 
   // Додаємо новий контакт до початкового масиву
-
-  await fs.appendFile(PATH_DB, JSON.stringify(newContact));
+  const updatedContacts = [...parsedContacts, newContact];
+  await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts));
 };
 
 await addOneContact();
